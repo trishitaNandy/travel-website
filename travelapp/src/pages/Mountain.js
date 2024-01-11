f2d4412b19b75a8dab96b202b6eabbf3e74c61d0
TrishitaNandy123 <trishitanandy@beyondthezenith.com>
2024-01-11 20:11:00 +0530

    second commit

--- travelapp/src/pages/Mountain.js
+++ travelapp/src/pages/Mountain.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import mountain_1 from '../assets/mountain_1.png'
 import MyNavbar from '../components/MyNavbar'
 
 const Mountain = () => {
   return (
-    <div>
+    <div style={{backgroundColor: 'blue'}}>
       <MyNavbar/>
-      <div className=' pt-20  pt-300'>Mountain</div>
+      <div className=' pt-20  pt-300' >Mountain</div>
   </div>
   )
 }
